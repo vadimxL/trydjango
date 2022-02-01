69ea2f5675d5fc7be01009a060abba9a9b773e3c
Vadim Malinovsky <vadim.malinovsky@servotronix.com>
2022-02-01 09:34:44 +0200

    update for tutorial no 14

--- trydjango/views.py
+++ trydjango/views.py
@@ -8,7 +8,12 @@ def home_view(request):
     Return HTML as a response (We pick to return the response)
     """
     name = "Vadim"
+    name2 = "Alona"
     number = random.randint(10,123123123) # Can be some API call to REST API...
+
+    # from database?
+    # article_name = ?
+    # article_content = ?
 
     # Django templates
     H1_STRING = f"""
@@ -17,6 +22,8 @@ def home_view(request):
 
     P_STRING = f"""
     <p>Hello {name} - {number}!</p>
+    <p>Hello {name2}!</p>
+    <img src="https://soviet-art.ru/wp-content/uploads/2016/03/Legendary-Soviet-chocolate-Alyonka-1.jpg" style="width:200px;height:300px;">
     """
     HTML_STRING = H1_STRING + P_STRING
     return HttpResponse(HTML_STRING)
